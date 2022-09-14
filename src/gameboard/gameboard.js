80e1227642980f847543e3f27c337a0f1ce61193
georgevictor90 <georgevictorl@yahoo.com>
2022-09-14 23:38:58 +0300

    space out ships on gameboards

--- src/gameboard/gameboard.js
+++ src/gameboard/gameboard.js
@@ -16,12 +16,6 @@ export default class Gameboard {
     return rows;
   }
   placeShip(head, shipType, orientation, name) {
-    // if (orientation === "horizontal" && 10 - head[0] < shipType.size)
-    //   throw new Error("Outside Board horizontal");
-
-    // if (orientation === "vertical" && 10 - head[1] < shipType.size)
-    //   throw new Error("Outside Board vertical");
-
     let ship = new Ship(shipType, head, orientation);
 
     this.spaces[head[1]][head[0]] = ship.name.charAt(0).toUpperCase();
@@ -29,12 +23,8 @@ export default class Gameboard {
     ship.position.slice(1).forEach((p) => {
       this.spaces[p[1]][p[0]] = ship.name.charAt(0);
     });
-
-    // console.log("position " + ship.position);
     ship.position.forEach((pos) => {
-      // console.log(document.getElementById(`${pos[0]}, ${pos[1]}`));
       if (document.getElementById(`${name}, ${pos[0]}, ${pos[1]}`) === null) {
-        console.log([pos[0], pos[1]]);
       } else {
         document
           .getElementById(`${name}, ${pos[0]}, ${pos[1]}`)
@@ -42,6 +32,16 @@ export default class Gameboard {
       }
     });
     this.ships.push(ship);
+    this.handleReservedSpaces(ship);
+  }
+
+  handleReservedSpaces(ship) {
+    let unfiltered = this.unfilteredReservedSpaces(
+      ship.position,
+      ship.orientation
+    );
+    let filtered = this.filterReserved(unfiltered);
+    this.markReservedOnGameboard(filtered);
   }
 
   receiveAttack(coords) {
@@ -59,7 +59,6 @@ export default class Gameboard {
       let ship = this.ships.find(
         (s) => s.name.charAt(0) === shipInitial.toLowerCase()
       );
-      // console.log(ship);
       ship.hit(coords);
       this.spaces[coords[1]][coords[0]] += " hit";
     }
@@ -67,5 +66,89 @@ export default class Gameboard {
 
   allSunk() {
     return this.ships.every((x) => x.isSunk() === true);
+  }
+
+  unfilteredReservedSpaces(positionsArray, orientation) {
+    //store head and tail of a ship in variables
+    const h = positionsArray[0];
+    const t = positionsArray[positionsArray.length - 1];
+    let topRow = [];
+    let bottomRow = [];
+    let leftCol = [];
+    let rightCol = [];
+    let result = [];
+
+    if (orientation === "horizontal") {
+      positionsArray.forEach((pos) => {
+        let newPos = [pos[0], pos[1] - 1];
+        topRow.push(newPos);
+      });
+
+      positionsArray.forEach((pos) => {
+        let newPos = [pos[0], pos[1] + 1];
+        bottomRow.push(newPos);
+      });
+
+      leftCol = [
+        [h[0] - 1, h[1] - 1],
+        [h[0] - 1, h[1]],
+        [h[0] - 1, h[1] + 1],
+      ];
+
+      rightCol = [
+        [t[0] + 1, t[1] - 1],
+        [t[0] + 1, t[1]],
+        [t[0] + 1, t[1] + 1],
+      ];
+    } else if (orientation === "vertical") {
+      positionsArray.forEach((pos) => {
+        let newPos = [pos[0] - 1, pos[1]];
+        leftCol.push(newPos);
+      });
+
+      positionsArray.forEach((pos) => {
+        let newPos = [pos[0] + 1, pos[1]];
+        rightCol.push(newPos);
+      });
+
+      topRow = [
+        [h[0] - 1, h[1] - 1],
+        [h[0], h[1] - 1],
+        [h[0] + 1, h[1] - 1],
+      ];
+
+      bottomRow = [
+        [t[0] - 1, t[1] + 1],
+        [t[0], t[1] + 1],
+        [t[0] + 1, t[1] + 1],
+      ];
+    }
+
+    result.push(topRow);
+    result.push(bottomRow);
+    result.push(leftCol);
+    result.push(rightCol);
+    return [...topRow, ...bottomRow, ...leftCol, ...rightCol];
+  }
+
+  filterReserved(unfilteredArray) {
+    console.log(unfilteredArray);
+    let filtered = [];
+
+    filtered = unfilteredArray.filter(
+      (arr) => arr[0] >= 0 && arr[0] <= 9 && arr[1] >= 0 && arr[1] <= 9
+    );
+    console.log(filtered);
+    return filtered;
+  }
+
+  markReservedOnGameboard(filteredArray) {
+    filteredArray.forEach((coord) => {
+      if (this.spaces[coord[1]][coord[0]] !== null) {
+        return;
+      } else {
+        this.spaces[coord[1]][coord[0]] = "r";
+      }
+    });
   }
 }
